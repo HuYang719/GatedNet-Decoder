86b19ee9e8b0974218d406129b3ceb90199117bb
HuYang719 <646228006@qq.com>
2018-10-23 03:04:35 +0800

    self-defined layers

--- layers/GatedLayer.py
+++ layers/GatedLayer.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-
 from .. import backend as K
 from .. import activations
 from .. import initializers
