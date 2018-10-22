a165b7f6387b5a1a8ba8488e837080383ef7978c
HuYang719 <646228006@qq.com>
2018-10-23 03:06:59 +0800

    BER and error number

--- Metrics/metrics.py
+++ Metrics/metrics.py
@@ -6,10 +6,12 @@ Author: Lucyyang
 import tensorflow as tf
 from keras import backend as K
 
+# error number
 def errors(y_true, y_pred):
     print(y_true,y_pred)
     return K.sum(tf.cast(K.not_equal(y_true, K.round(y_pred)),tf.int32))
-
+    
+# bit error rate
 def BER(y_true, y_pred):
     return K.mean(K.not_equal(y_true, K.round(y_pred)))
     return ber
